3460ff0d7ee30cda5ba8823613187b5783dc1d20
Alex Kurop <alkurop@gmail.com>
2021-10-04 21:56:52 +0200

    [Espresso Tests] Recorder smoke test

---
+++ app/src/androidTest/java/com/omar/retromp3recorder/app/test/RecordTest.kt
@@ -0,0 +1,37 @@
+package com.omar.retromp3recorder.app.test
+
+
+import android.Manifest
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.rule.GrantPermissionRule
+import com.omar.retromp3recorder.app.R
+import com.omar.retromp3recorder.app.ui.main.MainActivity
+import org.hamcrest.Matchers.not
+import org.junit.Rule
+import org.junit.Test
+
+
+class HelloWorldEspressoTest {
+
+    @get:Rule
+    val permissionsRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.RECORD_AUDIO
+    )
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun recordSmokeTest() {
+        onView(withId(R.id.acf_stop)).check(matches(isNotEnabled()))
+        onView(withId(R.id.acf_record)).perform(ViewActions.click())
+        onView(withId(R.id.acf_stop)).check(matches(isEnabled()))
+        onView(withId(R.id.acf_stop)).perform(ViewActions.click())
+        onView(withId(R.id.current_file_text)).check(matches(not(withText(R.string.no_file))))
+    }
+}
