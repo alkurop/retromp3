9cab8a3ce52d8f78b598de0eadb3743c6464add7
Alex Kurop <alkurop@gmail.com>
2021-10-16 20:20:13 +0300

    [BugBash] fix db crash

--- storage/src/main/java/com/omar/retromp3recorder/storage/db/FileDbEntity.kt
+++ storage/src/main/java/com/omar/retromp3recorder/storage/db/FileDbEntity.kt
@@ -16,9 +16,27 @@ data class FileDbEntity(
 @Entity
 data class WaveformDbEntity(
     @ColumnInfo(typeAffinity = ColumnInfo.BLOB)
-    val waveform: ByteArray,
-    val stepMillis: Int?
-)
+    val waveform: ByteArray = ByteArray(0),
+    val stepMillis: Int? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as WaveformDbEntity
+
+        if (!waveform.contentEquals(other.waveform)) return false
+        if (stepMillis != other.stepMillis) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = waveform.contentHashCode()
+        result = 31 * result + (stepMillis ?: 0)
+        return result
+    }
+}
 
 @Dao
 interface FileDbEntityDao {
