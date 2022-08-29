2494aa3b5695729c7006575e54c998035c2a41cc
Oleksii Kuropiatnyk <alkurop@gmail.com>
2022-08-29 14:53:53 +0200

    Increase chunk size to overcome failing db issue with too wavetable size

--- utils/src/main/java/com/omar/retromp3recorder/utils/Constants.kt
+++ utils/src/main/java/com/omar/retromp3recorder/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.omar.retromp3recorder.utils
 
 object Constants {
     const val MAIN_THREAD = "MAIN_THREAD"
-    const val PLAYER_TO_RECORDER_CONVERSION_MILLIS: Int = 50
+    const val PLAYER_TO_RECORDER_CONVERSION_MILLIS: Int = 100
 }
