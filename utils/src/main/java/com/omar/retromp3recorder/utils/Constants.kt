6dcae55244bf85ab6c707200cbc7d744ef7fa0eb
Oleksii Kuropiatnyk <alkurop@gmail.com>
2022-08-30 16:06:01 +0200

    Revert conversion rate

--- utils/src/main/java/com/omar/retromp3recorder/utils/Constants.kt
+++ utils/src/main/java/com/omar/retromp3recorder/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.omar.retromp3recorder.utils
 
 object Constants {
     const val MAIN_THREAD = "MAIN_THREAD"
-    const val PLAYER_TO_RECORDER_CONVERSION_MILLIS: Int = 100
-    const val RECORDING_LENGTH_MULTIPLIER = 18_000 // 1 hours at rate 10 frames per second
+    const val PLAYER_TO_RECORDER_CONVERSION_MILLIS: Int = 50
+    const val RECORDING_LENGTH_MULTIPLIER = 36_000 // 1 hours at rate 100 frames per second
 }
