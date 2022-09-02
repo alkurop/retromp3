2e70d1638785c468348ce04d5358e94281ea77bc
Alex Kuropiatnyk <alkurop@gmail.com>
2022-09-03 01:35:20 +0200

    Remove annyoing text view

--- app/src/main/java/com/omar/retromp3recorder/app/ui/joined_progress/JoinedProgressFragment.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/joined_progress/JoinedProgressFragment.kt
@@ -2,7 +2,6 @@ package com.omar.retromp3recorder.app.ui.joined_progress
 
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -12,14 +11,11 @@ import com.omar.retromp3recorder.app.uiutils.observe
 import com.omar.retromp3recorder.dto.JoinedProgress
 import com.omar.retromp3recorder.ui.wavetable.WavetablePreview
 import com.omar.retromp3recorder.ui.wavetable.WavetableSeekbarPreview
-import com.omar.retromp3recorder.utils.toDisplay
 import com.omar.retromp3recorder.utils.toSeekbarTime
-import com.omar.retromp3recorder.utils.toSpannableStringWithSmallMillis
 
 class JoinedProgressFragment : Fragment(R.layout.fragment_joined_progress) {
     private val viewModel by viewModels<JoinedProgressViewModel>()
     private val recorderWavetable by lazyView<WavetablePreview>(R.id.fjp_recorder_wavetable)
-    private val recorderProgress by lazyView<TextView>(R.id.fjp_recorder_progress)
     private val playerProgress by lazyView<WavetableSeekbarPreview>(R.id.fjp_player_progress)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,19 +38,13 @@ class JoinedProgressFragment : Fragment(R.layout.fragment_joined_progress) {
 
     private fun renderView(joinedProgress: JoinedProgress) {
         playerProgress.isGone = joinedProgress !is JoinedProgress.PlayerProgressShown
-        recorderProgress.isGone = joinedProgress !is JoinedProgress.RecorderProgressShown
         recorderWavetable.isGone = joinedProgress !is JoinedProgress.RecorderProgressShown
 
         when (joinedProgress) {
-            is JoinedProgress.RecorderProgressShown -> {
-                recorderProgress.text = joinedProgress.progress.toDisplay()
-                    .toSpannableStringWithSmallMillis(requireContext(), R.style.Control_Normal_Millis)
-                recorderWavetable.update(joinedProgress.wavetable.data)
-            }
+            is JoinedProgress.RecorderProgressShown -> recorderWavetable.update(joinedProgress.wavetable.data)
             is JoinedProgress.PlayerProgressShown -> {
                 val progress = joinedProgress.progress
-                progress.ghost?.let { p ->
-                    playerProgress.updateProgress(
+                progress.ghost?.let { p -> playerProgress.updateProgress(
                         p.progress.toSeekbarTime() to p.duration.toSeekbarTime()
                     )
                 }
