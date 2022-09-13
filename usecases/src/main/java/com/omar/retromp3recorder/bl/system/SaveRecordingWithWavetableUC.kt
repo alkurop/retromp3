69e1e00ac7a30c849273722e1b1371e28b9fdf4f
Alex Kuropiatnyk <alkurop@gmail.com>
2022-09-13 23:20:11 +0200

    Fix record update wavetable on main view

--- usecases/src/main/java/com/omar/retromp3recorder/bl/system/SaveRecordingWithWavetableUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/system/SaveRecordingWithWavetableUC.kt
@@ -2,6 +2,7 @@ package com.omar.retromp3recorder.bl.system
 
 import com.omar.retromp3recorder.storage.db.AppDatabase
 import com.omar.retromp3recorder.storage.db.toDatabaseEntity
+import com.omar.retromp3recorder.storage.repo.CurrentFileRepo
 import com.omar.retromp3recorder.storage.repo.WavetableRepo
 import com.omar.retromp3recorder.utils.*
 import io.reactivex.rxjava3.core.Completable
@@ -11,7 +12,8 @@ class SaveRecordingWithWavetableUC @Inject constructor(
     private val appDatabase: AppDatabase,
     private val wavetableRepo: WavetableRepo,
     private val fileLister: FileLister,
-    private val saveMp3TagsUC: SaveMp3TagsUC
+    private val saveMp3TagsUC: SaveMp3TagsUC,
+    private val currentFileRepo: CurrentFileRepo
 ) {
     fun execute(): Completable =
         wavetableRepo.observe()
@@ -25,6 +27,7 @@ class SaveRecordingWithWavetableUC @Inject constructor(
                             val newItem = fileLister.discoverFile(path)
                                 .copy(wavetable = wave, length = fileLister.discoverLength(path))
                             fileEntityDao.insert(listOf(newItem.toDatabaseEntity()))
+                            currentFileRepo.onNext(newItem.toOptional())
                         })
             }
 }
