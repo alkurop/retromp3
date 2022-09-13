5972889f19e5178691cb35602dac23538b3242c3
Alex Kuropiatnyk <alkurop@gmail.com>
2022-09-14 00:45:11 +0200

    Fix delete single item

--- usecases/src/main/java/com/omar/retromp3recorder/bl/files/DeleteCurrentFileUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/files/DeleteCurrentFileUC.kt
@@ -1,12 +1,15 @@
 package com.omar.retromp3recorder.bl.files
 
 import com.omar.retromp3recorder.dto.ExistingFileWrapper
+import com.omar.retromp3recorder.storage.db.AppDatabase
+import com.omar.retromp3recorder.storage.db.toDatabaseEntity
 import com.omar.retromp3recorder.storage.repo.CurrentFileRepo
 import com.omar.retromp3recorder.utils.FileDeleter
 import io.reactivex.rxjava3.core.Completable
 import javax.inject.Inject
 
 class DeleteCurrentFileUC @Inject constructor(
+    private val appDatabase: AppDatabase,
     private val currentFileRepo: CurrentFileRepo,
     private val fileDeleter: FileDeleter,
     private val takeLastFileUC: TakeLastFileFastUC
@@ -15,12 +18,18 @@ class DeleteCurrentFileUC @Inject constructor(
         return currentFileRepo
             .takeOne()
             .flatMapCompletable { optional ->
-                val filePath = (optional.value as? ExistingFileWrapper)?.path
+                val file = (optional.value as? ExistingFileWrapper)
 
                 Completable.fromAction {
-                    filePath?.let { fileDeleter.deleteFile(filePath) }
+                    file?.let {
+                        fileDeleter.deleteFile(it.path)
+                        appDatabase.fileEntityDao().delete(listOf(file.toDatabaseEntity()))
+                    }
+
                 }
             }
             .andThen(takeLastFileUC.execute())
     }
 }
+
+//todo fix take last file usecase
--- usecases/src/main/java/com/omar/retromp3recorder/bl/files/TakeLastFileDirScanUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/files/TakeLastFileDirScanUC.kt
@@ -3,7 +3,6 @@ package com.omar.retromp3recorder.bl.files
 import com.omar.retromp3recorder.bl.system.WaveformScanUpdaterUC
 import com.omar.retromp3recorder.dto.isEmpty
 import com.omar.retromp3recorder.storage.db.AppDatabase
-import com.omar.retromp3recorder.storage.db.FileDbEntity
 import com.omar.retromp3recorder.storage.db.toFileWrapper
 import com.omar.retromp3recorder.utils.Optional
 import io.reactivex.rxjava3.core.Completable
