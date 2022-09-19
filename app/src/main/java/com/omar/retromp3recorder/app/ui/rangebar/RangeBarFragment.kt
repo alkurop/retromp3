eb323fe7ff2e30d7f08b905117cf041366940e86
Alex Kuropiatnyk <alkurop@gmail.com>
2022-09-19 22:49:01 +0200

    Add range state setter

--- app/src/main/java/com/omar/retromp3recorder/app/ui/rangebar/RangeBarFragment.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/rangebar/RangeBarFragment.kt
@@ -8,6 +8,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.github.alkurop.rangebar.RangeBar
+import com.jakewharton.rxbinding4.view.clicks
 import com.omar.retromp3recorder.app.R
 import com.omar.retromp3recorder.app.ui.utils.lazyView
 import com.omar.retromp3recorder.app.uiutils.TimeDisplay.toDisplay
@@ -16,6 +17,7 @@ import com.omar.retromp3recorder.dto.PlayerRange
 
 class RangeBarFragment : Fragment(R.layout.fragment_rangebar) {
     private val rangeBar by lazyView<RangeBar>(R.id.range_bar)
+    private val rangeBarStateText by lazyView<TextView>(R.id.range_state)
     private val startView by lazyView<TextView>(R.id.range_start)
     private val endView by lazyView<TextView>(R.id.range_end)
     private val viewModel: RangeBarViewModel by viewModels()
@@ -23,6 +25,9 @@ class RangeBarFragment : Fragment(R.layout.fragment_rangebar) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.state.observe(viewLifecycleOwner, ::renderState)
+        rangeBarStateText.clicks().observe(viewLifecycleOwner) {
+            viewModel.input.onNext(RangeBarView.Input.Enable)
+        }
         rangeBar.setOnRangeBarChangeListener { tick ->
             viewModel.input.onNext(
                 RangeBarView.Input.RangeSet(
@@ -47,6 +52,8 @@ class RangeBarFragment : Fragment(R.layout.fragment_rangebar) {
                 endView.text = state.toMillis.toDisplay(requireContext())
                 val active = state.range.isActive
                 rangeBar.isActivated = active
+                val rangeState = getString(if (active) R.string.on else R.string.off)
+                rangeBarStateText.text = getString(R.string.range_enabled, rangeState)
             }
         }
     }
