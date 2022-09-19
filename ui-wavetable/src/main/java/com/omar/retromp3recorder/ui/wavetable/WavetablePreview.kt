7efbbb0482308aed451f90246db96b245e6c7bd2
Oleksii Kuropiatnyk <alkurop@gmail.com>
2022-09-19 11:58:48 +0200

    Add range drawing

--- ui-wavetable/src/main/java/com/omar/retromp3recorder/ui/wavetable/WavetablePreview.kt
+++ ui-wavetable/src/main/java/com/omar/retromp3recorder/ui/wavetable/WavetablePreview.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.omar.retromp3recorder.dto.PlayerRange
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import kotlin.math.max
 
@@ -16,10 +17,17 @@ class WavetablePreview @JvmOverloads constructor(
         style = Paint.Style.STROKE
         color = Color.rgb(0, 255, 0)
     }
-    private val bytesBus = BehaviorSubject.create<ByteArray>()
 
-    fun update(bytes: ByteArray) {
-        bytesBus.onNext(bytes)
+    private val paintRange: Paint = Paint().apply {
+        strokeWidth = 1f
+        style = Paint.Style.STROKE
+        color = Color.rgb(255, 191, 0)
+    }
+
+    private val bytesWithRangeBus = BehaviorSubject.create<BytesWithRange>()
+
+    fun update(bytesWithRange: BytesWithRange) {
+        bytesWithRangeBus.onNext(bytesWithRange)
         invalidate()
     }
 
@@ -27,12 +35,14 @@ class WavetablePreview @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        if (bytesBus.hasValue().not()) {
+        if (bytesWithRangeBus.hasValue().not()) {
             return
         }
-        val mBytes = bytesBus.blockingFirst()
+        val data = bytesWithRangeBus.blockingFirst()
+        val mBytes = data.bytes
         val length = mBytes.size - 1
         val path = Path()
+        val rangePath = Path()
 
         for (i in 0 until length) {
             val relativeLoudness = max(((mBytes[i]) * (height / 2) / Byte.MAX_VALUE), 1)
@@ -47,5 +57,45 @@ class WavetablePreview @JvmOverloads constructor(
             path.addRect(rect, Path.Direction.CCW)
         }
         canvas.drawPath(path, paint)
+
+        val range = data.range
+        if (range != null) {
+            val max = range.max
+            val rangeStart = RectF(
+                (width * (range.from) / max).toFloat() - 1,
+                0f,
+                (width * (range.from) / max).toFloat(),
+                height.toFloat(),
+            )
+            val rangeEnd = RectF(
+                (width * (range.to) / max).toFloat() - 1,
+                0f,
+                (width * (range.to) / max).toFloat(),
+                height.toFloat(),
+            )
+            rangePath.addRect(rangeStart, Path.Direction.CCW)
+            rangePath.addRect(rangeEnd, Path.Direction.CCW)
+            canvas.drawPath(rangePath, paintRange)
+        }
+    }
+}
+
+data class BytesWithRange(val bytes: ByteArray, val range: PlayerRange?) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BytesWithRange
+
+        if (!bytes.contentEquals(other.bytes)) return false
+        if (range != other.range) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = bytes.contentHashCode()
+        result = 31 * result + range.hashCode()
+        return result
     }
 }
