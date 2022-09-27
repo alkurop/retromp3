861e857748695e2481a5039bb3989d4b4c0fe8ea
Alex Kuropiatnyk <alkurop@gmail.com>
2022-09-28 00:08:39 +0200

    Deactivate player settings when record

--- usecases/src/main/java/com/omar/retromp3recorder/bl/audio/StartRecordUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/audio/StartRecordUC.kt
@@ -3,6 +3,7 @@ package com.omar.retromp3recorder.bl.audio
 import android.Manifest
 import android.media.AudioAttributes.USAGE_GAME
 import android.media.AudioAttributes.USAGE_MEDIA
+import com.omar.retromp3recorder.bl.enablers.DeactivatePlayerControlsUC
 import com.omar.retromp3recorder.bl.system.CheckPermissionsUC
 import com.omar.retromp3recorder.bl.system.RequestMediaProjectionUC
 import com.omar.retromp3recorder.iorecorder.Mp3VoiceRecorder
@@ -19,34 +20,37 @@ class StartRecordUC @Inject constructor(
     private val recorderPrefsRepo: RecorderPrefsRepo,
     private val checkPermissionsUC: CheckPermissionsUC,
     private val captureCompletableCreator: CaptureCompletableCreator,
+    private val deactivatePlayerControlsUC: DeactivatePlayerControlsUC,
     private val permissionsRequestBus: PermissionsRequestBus,
     private val projectionRepo: MediaProjectionStateRepo,
     private val requestMediaProjectionUC: RequestMediaProjectionUC,
     private val serviceDealer: ServiceDealer,
 ) {
     fun execute(): Completable {
-        fun executeMedia(source: Int) = Completable
-            .fromAction { serviceDealer.startMediaProjectionService() }
-            .andThen(projectionRepo.observe().takeOne())
-            .flatMapCompletable {
-                val projection = it.mediaProjection.value
-                if (projection != null) {
-                    captureCompletableCreator.create(
-                        Mp3VoiceRecorder.AudioSource.Output(
-                            projection,
-                            source
+        fun executeMedia(source: Int) =
+            Completable
+                .fromAction { serviceDealer.startMediaProjectionService() }
+                .andThen(projectionRepo.observe().takeOne())
+                .flatMapCompletable {
+                    val projection = it.mediaProjection.value
+                    if (projection != null) {
+                        captureCompletableCreator.create(
+                            Mp3VoiceRecorder.AudioSource.Output(
+                                projection,
+                                source
+                            )
                         )
-                    )
-                } else {
-                    requestMediaProjectionUC.execute()
+                    } else {
+                        requestMediaProjectionUC.execute()
+                    }
                 }
-            }
 
         fun executeMic() = captureCompletableCreator.create(Mp3VoiceRecorder.AudioSource.Mic)
 
         val abort = Completable.complete()
-        return checkPermissionsUC
-            .execute(voiceRecordPermissions)
+        return deactivatePlayerControlsUC
+            .execute()
+            .andThen(checkPermissionsUC.execute(voiceRecordPermissions))
             .andThen(permissionsRequestBus.observe().takeOne())
             .flatMapCompletable { shouldAskPermissions ->
                 if (shouldAskPermissions is ShouldRequestPermissions.Granted) {
---
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/enablers/DeactivatePlayerControlsUC.kt
@@ -0,0 +1,24 @@
+package com.omar.retromp3recorder.bl.enablers
+
+import com.omar.retromp3recorder.dto.PlayerControls
+import com.omar.retromp3recorder.storage.repo.local.PlayerControlsRepo
+import io.reactivex.rxjava3.core.Completable
+import javax.inject.Inject
+
+class DeactivatePlayerControlsUC @Inject constructor(
+    private val playerControlsRepo: PlayerControlsRepo
+) {
+    fun execute(): Completable = playerControlsRepo.takeOne()
+        .flatMapCompletable { (loop, range, reverse, speed) ->
+            Completable.fromAction {
+                playerControlsRepo.onNext(
+                    PlayerControls(
+                        loop.copy(isEnabled = false),
+                        range.copy(isVisible = false),
+                        reverse.copy(isEnabled = false),
+                        speed.copy(isEnabled = false)
+                    )
+                )
+            }
+        }
+}
