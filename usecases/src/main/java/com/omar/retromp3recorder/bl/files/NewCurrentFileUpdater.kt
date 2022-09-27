3f31d8fba74e30ec20af9f2e5e9a20585c9b08ec
Alex Kuropiatnyk <alkurop@gmail.com>
2022-09-28 00:19:07 +0200

    Deactivate player settings when changed file

--- usecases/src/main/java/com/omar/retromp3recorder/bl/files/NewCurrentFileUpdater.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/files/NewCurrentFileUpdater.kt
@@ -1,6 +1,7 @@
 package com.omar.retromp3recorder.bl.files
 
 import com.github.alkurop.ghostinshell.Shell
+import com.omar.retromp3recorder.bl.enablers.DeactivatePlayerControlsUC
 import com.omar.retromp3recorder.dto.PlayerProgress
 import com.omar.retromp3recorder.dto.PlayerRange
 import com.omar.retromp3recorder.storage.repo.local.PlayerProgressRepo
@@ -16,26 +17,30 @@ class NewCurrentFileUpdater @Inject constructor(
     private val hasPlayableFileMapper: HasPlayableFileMapper,
     private val playerProgressRepo: PlayerProgressRepo,
     private val rangeBarResetBus: RangeBarResetBus,
+    private val deactivatePlayerControlsUC: DeactivatePlayerControlsUC,
     private val scheduler: Scheduler
 ) {
-    fun execute(): Completable = hasPlayableFileMapper.observe()
-        .map { currentFile ->
-            val unwrappedFile = currentFile.value
-            if (unwrappedFile != null) {
-                PlayerProgressRepo.In.NewCurrentFile(
-                    PlayerProgress(
-                        0,
-                        unwrappedFile.length!!,
-                        PlayerRange()
+    fun execute(): Completable =
+        hasPlayableFileMapper.observe()
+            .map { currentFile ->
+                val unwrappedFile = currentFile.value
+                if (unwrappedFile != null) {
+                    PlayerProgressRepo.In.NewCurrentFile(
+                        PlayerProgress(
+                            0,
+                            unwrappedFile.length!!,
+                            PlayerRange()
+                        )
                     )
-                )
-            } else PlayerProgressRepo.In.Hidden
-        }
-        .flatMapCompletable { progress ->
-            Completable.fromAction {
-                playerProgressRepo.onNext(progress)
-                rangeBarResetBus.onNext(Shell(0))
+                } else PlayerProgressRepo.In.Hidden
             }
-        }
-        .subscribeOn(scheduler)
+            .flatMapCompletable { progress ->
+                Completable
+                    .fromAction {
+                        playerProgressRepo.onNext(progress)
+                        rangeBarResetBus.onNext(Shell(0))
+                    }
+                    .andThen(deactivatePlayerControlsUC.execute())
+            }
+            .subscribeOn(scheduler)
 }
