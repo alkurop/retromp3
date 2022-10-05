510146cfb4342c2b5ec3212776318e49bcbdd749
Alex Kuropiatnyk <alkurop@gmail.com>
2022-10-05 21:41:51 +0200

    Revert unwanted changes from cherry pick

--- io-audio-tansformer/src/main/java/com/omar/retromp3recorder/io/audiotransformer/AudioCropper.kt
+++ io-audio-tansformer/src/main/java/com/omar/retromp3recorder/io/audiotransformer/AudioCropper.kt
@@ -2,27 +2,21 @@ package com.omar.retromp3recorder.io.audiotransformer
 
 import com.arthenica.mobileffmpeg.Config
 import com.arthenica.mobileffmpeg.FFmpeg
-import com.omar.retromp3recorder.dto.ExistingFileWrapper
 import com.omar.retromp3recorder.dto.FromToMillis
-import com.omar.retromp3recorder.dto.NewNameSuggestion
 import javax.inject.Inject
 
 
 class AudioCropper @Inject constructor() {
-    fun crop(request: CropRequest): CropResponse {
+    fun crop(request: CropRequest): Boolean {
         val returnCode = FFmpeg.execute(
-            "-ss ${request.range.from}ms " + "-to ${request.range.to}ms -i " + "${request.existingFileWrapper.path} -c:a " + "copy $${request.nameSuggestion.path}"
+            "-ss ${request.fromToMillis.from}ms -to ${request.fromToMillis.to}ms -i ${request.originalFilePath} -c:a copy $${request.newFilePath}"
         )
-        return CropResponse(Config.RETURN_CODE_CANCEL != returnCode)
+        return Config.RETURN_CODE_CANCEL != returnCode
     }
 }
 
 data class CropRequest(
-    val range: FromToMillis,
-    val existingFileWrapper: ExistingFileWrapper,
-    val nameSuggestion: NewNameSuggestion
-)
-
-data class CropResponse(
-    val isSuccess: Boolean
+    val fromToMillis: FromToMillis,
+    val originalFilePath: String,
+    val newFilePath: String
 )
--- usecases/src/main/java/com/omar/retromp3recorder/bl/actions/CropUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/actions/CropUC.kt
@@ -1,11 +1,15 @@
 package com.omar.retromp3recorder.bl.actions
 
-import com.omar.retromp3recorder.bl.waveform.WaveformScanner
+import com.omar.retromp3recorder.bl.audio.JoinedProgressMapper
+import com.omar.retromp3recorder.bl.files.GetCropFileNameUC
+import com.omar.retromp3recorder.dto.ExistingFileWrapper
+import com.omar.retromp3recorder.dto.JoinedProgress
 import com.omar.retromp3recorder.io.audiotransformer.AudioCropper
-import com.omar.retromp3recorder.io.audiotransformer.CropRequest
-import com.omar.retromp3recorder.io.audiotransformer.CropResponse
 import com.omar.retromp3recorder.storage.db.AppDatabase
 import com.omar.retromp3recorder.storage.repo.local.CurrentFileRepo
+import com.omar.retromp3recorder.utils.takeOne
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
@@ -16,18 +20,22 @@ class CropUC @Inject constructor(
     private val appDatabase: AppDatabase,
     private val audioCropper: AudioCropper,
     private val currentFileRepo: CurrentFileRepo,
-    private val waveformScanner: WaveformScanner,
-
+    private val getCropFileNameUC: GetCropFileNameUC,
+    private val joinedProgressRepo: JoinedProgressMapper,
+    private val scheduler: Scheduler
 ) {
-    fun execute(request: CropRequest): Single<CropResponse> = Single
-        .fromCallable {
-            audioCropper.crop(request)
-        }.flatMap {
-            if (it.isSuccess.not()) {
-                Single.just(it)
-            } else Single.just(it)
+    fun execute(): Completable =
+        Single.zip(
+            joinedProgressRepo.observe().takeOne(),
+            currentFileRepo.takeOne()
+        ) { progress, optional ->
+            val range = (progress as JoinedProgress.PlayerProgressShown).progress.range
+
+            val fileWrapper = (optional.value!! as ExistingFileWrapper)
+            val currentPath = fileWrapper.path
+            getCropFileNameUC.execute(currentPath).map { currentPath to it }
+        }.flatMapCompletable {
+            Completable.complete()
+//            audioCropper.crop()
         }
 }
-
-
-
