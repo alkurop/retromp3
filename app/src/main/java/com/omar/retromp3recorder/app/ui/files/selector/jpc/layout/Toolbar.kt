45e85d8b2fb81ebf3db0087c733b639b8891c9fb
Alex Kuropiatnyk <alkurop@gmail.com>
2022-10-15 17:27:46 +0200

    toolbar

--- app/src/main/java/com/omar/retromp3recorder/app/ui/files/selector/jpc/layout/Toolbar.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/files/selector/jpc/layout/Toolbar.kt
@@ -1,8 +1,19 @@
 package com.omar.retromp3recorder.app.ui.files.selector.jpc.layout
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
@@ -13,3 +24,37 @@ fun Toolbar(
     val input = remember { mutableListOf<String>() }
 
 }
+
+@Composable
+private fun AppBar(
+    backgroundColor: Color,
+    contentColor: Color,
+    elevation: Dp,
+    contentPadding: PaddingValues,
+    shape: Shape,
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    Surface(
+        color = backgroundColor,
+        contentColor = contentColor,
+        elevation = elevation,
+        shape = shape,
+        modifier = modifier
+    ) {
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Row(
+                Modifier.fillMaxWidth()
+                    .padding(contentPadding)
+                    .height(AppBarHeight),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                content = content
+            )
+        }
+    }
+}
+
+private val AppBarHeight = 56.dp
+// TODO: this should probably be part of the touch target of the start and end icons, clarify this
+private val AppBarHorizontalPadding = 4.dp
