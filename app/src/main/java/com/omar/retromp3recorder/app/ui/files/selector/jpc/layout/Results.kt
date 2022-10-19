1345a4ea47377db8054dd433848c64db3f06c75a
Alex Kuropiatnyk <alkurop@gmail.com>
2022-10-19 20:58:20 +0200

    Search toolbar coonect

--- app/src/main/java/com/omar/retromp3recorder/app/ui/files/selector/jpc/layout/Results.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/files/selector/jpc/layout/Results.kt
@@ -4,5 +4,5 @@ import androidx.compose.runtime.Composable
 
 
 @Composable
-fun Results() {
+fun Results(searchQuery: () -> String = {"HEllo"}) {
 }
--- app/src/main/java/com/omar/retromp3recorder/app/ui/files/selector/jpc/layout/SearchLayout.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/files/selector/jpc/layout/SearchLayout.kt
@@ -2,16 +2,19 @@ package com.omar.retromp3recorder.app.ui.files.selector.jpc.layout
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import com.omar.retromp3recorder.app.ui.RetroTheme
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun SearchLayout() {
     RetroTheme {
+        var query by remember { mutableStateOf("") }
         Column {
-            SearchToolbar()
-            Results()
+            SearchToolbar {
+                query = it
+            }
+            Results { query }
         }
     }
 }
