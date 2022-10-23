385611a06b8fdb288156514c8605bf07cc6f19e9
Alex Kuropiatnyk <alkurop@gmail.com>
2022-10-23 03:24:47 +0200

    Update file name

--- usecases/src/main/java/com/omar/retromp3recorder/bl/files/GetNewFileNameUC.kt
+++ usecases/src/main/java/com/omar/retromp3recorder/bl/files/GetNewFileNameUC.kt
@@ -4,7 +4,6 @@ import android.content.SharedPreferences
 import com.omar.retromp3recorder.storage.SharedPrefsKeys
 import com.omar.retromp3recorder.utils.DirPathProvider
 import io.reactivex.rxjava3.core.Single
-import java.util.*
 import javax.inject.Inject
 
 /**
@@ -15,22 +14,12 @@ class GetNewFileNameUC @Inject constructor(
     private val sharedPreferences: SharedPreferences
 ) {
     fun execute(): Single<String> {
-        val calendar = Calendar.getInstance()
-        calendar.timeInMillis = System.currentTimeMillis()
-        val hours = calendar.get(Calendar.HOUR_OF_DAY)
-        val minutes = calendar.get(Calendar.MINUTE)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-        val month = calendar.get(Calendar.MONTH)
-        val year = calendar.get(Calendar.YEAR)
-        val dateFormat =
-            "${day}-${month}-${year}_$hours-$minutes"
-
         return Single.fromCallable {
             val int = sharedPreferences.getInt(
                 SharedPrefsKeys.FILE_NAME,
                 1
             )
-            "${dirPathProvider.providerDirPath()}/${dateFormat}($int).mp3"
+            "${dirPathProvider.providerDirPath()}/audiorecord_$int.mp3"
         }
     }
 }
