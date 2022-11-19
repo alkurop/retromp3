dacca2a8acde2642779e8a776b7f2404cf3a6ed8
Alex Kuropiatnyk <alkurop@gmail.com>
2022-11-19 18:09:46 +0100

    Disable search when no file present

--- app/src/main/java/com/omar/retromp3recorder/app/ui/menu/container/logic/MenuStateExcavator.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/ui/menu/container/logic/MenuStateExcavator.kt
@@ -4,6 +4,7 @@ import com.omar.retromp3recorder.bl.audio.AudioState
 import com.omar.retromp3recorder.bl.audio.AudioStateMapper
 import com.omar.retromp3recorder.dto.MenuExecutable
 import com.omar.retromp3recorder.dto.VisibilityEnabler
+import com.omar.retromp3recorder.storage.repo.local.CurrentFileRepo
 import com.omar.retromp3recorder.storage.repo.local.MenuPopupBus
 import com.omar.retromp3recorder.storage.repo.local.PlayerControlsRepo
 import io.reactivex.rxjava3.core.Observable
@@ -12,35 +13,39 @@ import javax.inject.Inject
 class MenuStateExcavator @Inject constructor(
     private val playerControlsRepo: PlayerControlsRepo,
     private val menuPopupBus: MenuPopupBus,
-    private val audioStateMapper: AudioStateMapper
-) {
+    private val audioStateMapper: AudioStateMapper,
+    private val currentFileRepo: CurrentFileRepo,
+
+    ) {
     @Suppress("unused")
-    fun observe(): Observable<MenuContract.State> = Observable
-        .combineLatest(
-            playerControlsRepo.observe()
-                .map { (_, range, _, _) ->
-                    listOfNotNull(
-                        MenuContract.Item.Enable(
-                            VisibilityEnabler.RangeBar,
-                            range.isVisible
-                        ),
-                        MenuContract.Item.Popup(
-                            MenuExecutable.Crop,
-                            range.isVisible
-                        ),
-                        MenuContract.Item.Popup(
-                            MenuExecutable.Search,
-                            true
+    fun observe(): Observable<MenuContract.State> = currentFileRepo.observe().flatMap { file ->
+        Observable
+            .combineLatest(
+                playerControlsRepo.observe()
+                    .map { (_, range, _, _) ->
+                        listOfNotNull(
+                            MenuContract.Item.Enable(
+                                VisibilityEnabler.RangeBar,
+                                range.isVisible
+                            ),
+                            MenuContract.Item.Popup(
+                                MenuExecutable.Crop,
+                                range.isVisible
+                            ),
+                            MenuContract.Item.Popup(
+                                MenuExecutable.Search,
+                                file.value?.path != null
+                            )
                         )
-                    )
-                },
-            menuPopupBus.observe(),
-            audioStateMapper.observe()
-        ) { menu, popup, audioState ->
-            MenuContract.State(
-                items = menu,
-                isVisible = audioState != AudioState.Recording,
-                popup = popup
-            )
-        }
+                    },
+                menuPopupBus.observe(),
+                audioStateMapper.observe()
+            ) { menu, popup, audioState ->
+                MenuContract.State(
+                    items = menu,
+                    isVisible = audioState != AudioState.Recording,
+                    popup = popup
+                )
+            }
+    }
 }
