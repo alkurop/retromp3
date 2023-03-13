12368317f028042dae7ceb94a3d971cb2d1eaaf7
Alex Kuropiatnyk <alkurop@gmail.com>
2023-03-13 23:54:45 +0100

    Create range bar state mapper

---
+++ app/src/main/java/com/omar/retromp3recorder/app/screens/main/components/rangebar/RangeBarStateMapperFlow.kt
@@ -0,0 +1,40 @@
+package com.omar.retromp3recorder.app.screens.main.components.rangebar
+
+import com.omar.retromp3recorder.bl.audio.JoinedProgressMapper
+import com.omar.retromp3recorder.domain.JoinedProgress
+import com.omar.retromp3recorder.storage.repo.local.PlayerControlsRepo
+import com.omar.retromp3recorder.storage.repo.local.RangeBarResetBus
+import com.omar.retromp3recorder.utils.domain.toFromToMillis
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.rx3.asFlow
+import javax.inject.Inject
+
+class RangeBarStateMapperFlow @Inject constructor(
+    private val joinedProgressRepo: JoinedProgressMapper,
+    private val playerControlsRepo: PlayerControlsRepo,
+    private val rangeBarResetBus: RangeBarResetBus,
+
+    ) {
+    fun observe(): Flow<RangeBarView.State> {
+        return combine(
+            joinedProgressRepo.observe().asFlow(),
+            playerControlsRepo.flow(),
+            rangeBarResetBus.flow(),
+        ) { progress, features, reset ->
+            when {
+                progress is JoinedProgress.PlayerProgressShown && features.range.isVisible -> {
+                    val duration = progress.progress.duration
+                    val range = progress.progress.range
+                    RangeBarView.State.Visible(
+                        range = range,
+                        fromToMillis = range.toFromToMillis(duration),
+                        isActive = features.range.isActive,
+                        reset
+                    )
+                }
+                else -> RangeBarView.State.Hidden
+            }
+        }
+    }
+}
---
+++ app/src/test/java/com/omar/retromp3recorder/app/screens/main/components/rangebar/RangeBarStateMapperFlowTest.kt
@@ -0,0 +1,5 @@
+package com.omar.retromp3recorder.app.screens.main.components.rangebar
+
+import org.junit.Assert.*
+
+class RangeBarStateMapperFlowTest
