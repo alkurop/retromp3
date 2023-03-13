12505a3e2f9484af3b32304aa47a62340cb67497
Alex Kuropiatnyk <alkurop@gmail.com>
2023-03-13 13:40:43 +0100

    Change bitrate UC tests

---
+++ usecases/src/test/java/com/omar/retromp3recorder/bl/settings/ChangeBitrateUCTest.kt
@@ -0,0 +1,55 @@
+package com.omar.retromp3recorder.bl.settings
+
+import android.content.SharedPreferences
+import com.omar.retromp3recorder.iorecorder.Mp3VoiceRecorder
+import com.omar.retromp3recorder.storage.SharedPrefsKeys
+import com.omar.retromp3recorder.storage.repo.global.RecorderPrefsRepo
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verifyOrder
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ChangeBitrateUCTest {
+    private lateinit var repo: RecorderPrefsRepo
+    private val sharedPreferences: SharedPreferences = mockk()
+    private val editor: SharedPreferences.Editor = mockk(relaxed = true)
+    private lateinit var useCase: ChangeBitrateUC
+
+
+    @Before
+    fun setUp() {
+        repo =
+            RecorderPrefsRepo().apply { this.tryNext(Mp3VoiceRecorder.RecorderPrefs()) }
+        useCase = ChangeBitrateUC(repo, sharedPreferences)
+
+        every { sharedPreferences.edit() } returns editor
+        every { editor.putInt(any(), any()) } returns editor
+    }
+
+    @Test
+    fun `update update into shared prefs`() = runTest {
+        val setting = Mp3VoiceRecorder.BitRate._160
+        useCase.execute(setting)
+
+        verifyOrder {
+            editor.putInt(SharedPrefsKeys.BIT_RATE, setting.ordinal)
+            editor.apply()
+        }
+    }
+
+    @Test
+    fun `update put update into repo`() = runTest {
+        val setting = Mp3VoiceRecorder.BitRate._160
+
+        useCase.execute(setting)
+
+        val first = repo.flow().first().bitRate
+        assertEquals(first, setting)
+    }
+}
