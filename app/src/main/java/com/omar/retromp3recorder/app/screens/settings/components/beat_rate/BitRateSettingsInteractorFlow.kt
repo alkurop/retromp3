bebab4efad2c4f069d66a9432f3f8d8e644d14dc
Alex Kuropiatnyk <alkurop@gmail.com>
2023-03-15 09:39:40 +0100

    Fix log layput

--- app/src/main/java/com/omar/retromp3recorder/app/screens/settings/components/beat_rate/BitRateSettingsInteractorFlow.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/screens/settings/components/beat_rate/BitRateSettingsInteractorFlow.kt
@@ -4,24 +4,19 @@ import com.omar.retromp3recorder.bl.settings.ChangeBitrateUC
 import com.omar.retromp3recorder.iorecorder.Mp3VoiceRecorder
 import com.omar.retromp3recorder.storage.repo.global.RecorderPrefsRepo
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 class BitRateSettingsInteractorFlow @Inject constructor(
     private val changeBitrateUC: ChangeBitrateUC,
     private val recorderPrefsRepo: RecorderPrefsRepo,
-    dispatcher: CoroutineDispatcher,
-) : CoroutineScope {
-    override val coroutineContext: CoroutineContext = dispatcher + Job()
-
+    private val dispatcher: CoroutineDispatcher,
+) {
     fun processIO(upstream: Flow<Mp3VoiceRecorder.BitRate>): Flow<Mp3VoiceRecorder.BitRate> {
         return listOf(
             upstream.processInputs(),
             listenToRepos()
-        ).merge().flowOn(coroutineContext).distinctUntilChanged()
+        ).merge().flowOn(dispatcher)
     }
 
     private fun Flow<Mp3VoiceRecorder.BitRate>.processInputs(): Flow<Mp3VoiceRecorder.BitRate> {
