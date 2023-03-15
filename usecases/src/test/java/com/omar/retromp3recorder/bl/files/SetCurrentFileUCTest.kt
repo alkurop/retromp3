41b48e09a5b1359b7e08e845c2c5e9f765e3beb3
Alex Kuropiatnyk <alkurop@gmail.com>
2023-03-15 08:06:17 +0100

    Set current file tests

--- usecases/src/test/java/com/omar/retromp3recorder/bl/files/SetCurrentFileUCTest.kt
+++ usecases/src/test/java/com/omar/retromp3recorder/bl/files/SetCurrentFileUCTest.kt
@@ -1,9 +1,31 @@
 package com.omar.retromp3recorder.bl.files
 
+import com.omar.retromp3recorder.data.mock.MockFileFactory
+import com.omar.retromp3recorder.storage.repo.local.CurrentFileRepo
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class SetCurrentFileUCTest {
+
+    private lateinit var currentFileRepo: CurrentFileRepo
+    private lateinit var tested: SetCurrentFileUC
+
+    @Before
+    fun setUp() {
+        currentFileRepo = CurrentFileRepo()
+        tested = SetCurrentFileUC(currentFileRepo)
+    }
+
     @Test
-    fun stub() = fail()
+    fun `new existing file was set`() = runTest {
+        val file = MockFileFactory.giveExistingFile()
+        tested.execute(file)
+
+        assertEquals(file, currentFileRepo.first().value)
+    }
+
 }
