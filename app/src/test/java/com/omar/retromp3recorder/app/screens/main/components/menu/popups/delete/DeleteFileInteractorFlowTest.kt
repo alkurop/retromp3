32e3e0088b56040068500a1f2a037ff91bc7b7a4
Alex Kuropiatnyk <alkurop@gmail.com>
2023-03-17 14:02:43 +0100

    Name suggestion UC tests

--- app/src/test/java/com/omar/retromp3recorder/app/screens/main/components/menu/popups/delete/DeleteFileInteractorFlowTest.kt
+++ app/src/test/java/com/omar/retromp3recorder/app/screens/main/components/menu/popups/delete/DeleteFileInteractorFlowTest.kt
@@ -3,4 +3,7 @@ package com.omar.retromp3recorder.app.screens.main.components.menu.popups.delete
 import org.junit.Assert.*
 import org.junit.Test
 
-class DeleteFileInteractorFlowTest{@Test fun stub() = fail() }
+class DeleteFileInteractorFlowTest {
+    @Test
+    fun stub() = fail()
+}
--- usecases/src/test/java/com/omar/retromp3recorder/bl/crop/GenerateFileNameUCTest.kt
+++ usecases/src/test/java/com/omar/retromp3recorder/bl/crop/GenerateFileNameUCTest.kt
@@ -1,9 +1,49 @@
 package com.omar.retromp3recorder.bl.crop
 
+import com.omar.retromp3recorder.bl.files.GetCropFileNameUC
+import com.omar.retromp3recorder.data.mock.MockFileFactory
+import com.omar.retromp3recorder.data.mock.MockSuggestionFactory
+import com.omar.retromp3recorder.storage.repo.local.CurrentFileRepo
+import com.omar.retromp3recorder.utils.platform.toOptional
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.rxjava3.core.Single
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class GenerateFileNameUCTest {
-    @Test()
-    fun stub() = fail()
+    private lateinit var currentFileRepo: CurrentFileRepo
+    private val cropGeneratorUC = mockk<GetCropFileNameUC>()
+    private lateinit var tested: GenerateFileNameUC
+    private val nameSuggestion = MockSuggestionFactory.giveTestSuggestion()
+
+    @Before
+    fun setUp() {
+        currentFileRepo = CurrentFileRepo()
+        tested = GenerateFileNameUC(currentFileRepo, cropGeneratorUC)
+
+        every { cropGeneratorUC.execute(any()) } returns Single.just(nameSuggestion)
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException::class)
+    fun `WHEN current file empty THEN crash`() = runTest {
+        tested.execute()
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException::class)
+    fun `WHEN current file not current THEN crash`() = runTest {
+        currentFileRepo.emit(MockFileFactory.giveFutureFile().toOptional())
+        tested.execute()
+    }
+
+    @Test
+    fun `WHEN current file existing THEN generate file`() = runTest {
+        currentFileRepo.emit(MockFileFactory.giveExistingFile().toOptional())
+        val result = tested.execute()
+        assertEquals(nameSuggestion, result)
+    }
 }
