5592ee5207ae7bf97a64e3b7e5a96507d7a11d78
Alex Kuropiatnyk <alkurop@gmail.com>
2023-03-24 12:34:34 +0100

    UpdateMediaProjection tests

--- usecases/src/test/java/com/omar/retromp3recorder/bl/audio/record/RecordMicUCTest.kt
+++ usecases/src/test/java/com/omar/retromp3recorder/bl/audio/record/RecordMicUCTest.kt
@@ -1,9 +1,31 @@
 package com.omar.retromp3recorder.bl.audio.record
 
-import org.junit.Assert.*
+import io.mockk.coVerifySequence
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Test
 
-class RecordMicUCTest{
+@OptIn(ExperimentalCoroutinesApi::class)
+class RecordMicUCTest {
+    private val micCaptureCompletableCreator = mockk<AudioCaptureUC>(relaxed = true)
+    private val wavetableUC = mockk<RecordWavetableUCSuspend>(relaxed = true)
+
+    private lateinit var tested: RecordMicUC
+
+    @Before
+    fun setUp() {
+        tested = RecordMicUC(micCaptureCompletableCreator, wavetableUC)
+    }
+
     @Test
-    fun stub() = fail()
+    fun `on execute sequence executed`() = runTest {
+        tested.execute()
+
+        coVerifySequence {
+            micCaptureCompletableCreator.execute(any())
+            wavetableUC.execute()
+        }
+    }
 }
--- usecases/src/test/java/com/omar/retromp3recorder/bl/audio/record/UpdateMediaProjectionUCSuspendTest.kt
+++ usecases/src/test/java/com/omar/retromp3recorder/bl/audio/record/UpdateMediaProjectionUCSuspendTest.kt
@@ -1,10 +1,55 @@
 package com.omar.retromp3recorder.bl.audio.record
 
-import org.junit.Assert.fail
+import android.media.projection.MediaProjection
+import com.omar.retromp3recorder.bl.audio.actions.StartRecordUCSuspend
+import com.omar.retromp3recorder.storage.repo.global.MediaProjectionStateRepo
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
-class UpdateMediaProjectionUCSuspendTest{
+@OptIn(ExperimentalCoroutinesApi::class)
+class UpdateMediaProjectionUCSuspendTest {
+    private lateinit var mediaProjectionRepo: MediaProjectionStateRepo
+    private val startRecordUC = mockk<StartRecordUCSuspend>(relaxed = true)
+
+    private lateinit var tested: UpdateMediaProjectionUCSuspend
+
+    @Before
+    fun setUp() {
+        mediaProjectionRepo = MediaProjectionStateRepo()
+        tested = UpdateMediaProjectionUCSuspend(mediaProjectionRepo, startRecordUC)
+    }
+
     @Test
-    fun stub() = fail()
+    fun `on execute projection repo updated`() = runTest {
+        val projection = mockk<MediaProjection>()
+
+        tested.execute(projection)
+
+        assertEquals(projection, mediaProjectionRepo.first().mediaProjection.value)
+    }
+
+    @Test
+    fun `if has projection start record executed`() = runTest {
+        val projection = mockk<MediaProjection>()
+
+        tested.execute(projection)
+
+        assertEquals(projection, mediaProjectionRepo.first().mediaProjection.value)
+        coVerify { startRecordUC.execute() }
+    }
+
+    @Test
+    fun `if has no projection start record no executed`() = runTest {
+
+        tested.execute(null)
+
+        assertEquals(null, mediaProjectionRepo.first().mediaProjection.value)
+        coVerify(exactly = 0) { startRecordUC.execute() }
+    }
 
 }
