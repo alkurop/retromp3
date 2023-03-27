1b15651e3983c39b6db38da372c370fcc6e40a5c
Alex Kuropiatnyk <alkurop@gmail.com>
2023-03-28 00:18:11 +0200

    Feature flag usecase tests

--- usecases/src/test/java/com/omar/retromp3recorder/bl/settings/FeatureMapLoadUCSuspendTest.kt
+++ usecases/src/test/java/com/omar/retromp3recorder/bl/settings/FeatureMapLoadUCSuspendTest.kt
@@ -1,9 +1,52 @@
 package com.omar.retromp3recorder.bl.settings
 
-import org.junit.Assert.*
+import android.content.SharedPreferences
+import com.omar.retromp3recorder.domain.FeatureFlag
+import com.omar.retromp3recorder.domain.FeatureFlagSetting
+import com.omar.retromp3recorder.storage.repo.global.FeatureFlagRepo
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
-class FeatureMapLoadUCSuspendTest{
+@OptIn(ExperimentalCoroutinesApi::class)
+@Suppress("UNUSED")
+class FeatureMapLoadUCSuspendTest {
+    lateinit var featureFlagRepo: FeatureFlagRepo
+
+    @MockK
+    lateinit var sharedPreferences: SharedPreferences
+
+    @InjectMockKs
+    lateinit var tested: FeatureMapLoadUCSuspend
+
+    @Before
+    fun setUp() {
+        featureFlagRepo = FeatureFlagRepo()
+        MockKAnnotations.init(this)
+        every { sharedPreferences.getBoolean(any(), any()) } returns true
+    }
+
     @Test
-    fun stub() = fail()
+    fun `for each key shared pref is pulled`() = runTest {
+        tested.execute()
+        FeatureFlag.values().forEach {
+            verify { sharedPreferences.getBoolean(it.key, it.isEnabledByDefault) }
+        }
+    }
+
+    @Test
+    fun `result map emitted with all keys`() = runTest {
+        tested.execute()
+
+        val expected = FeatureFlag.values().associateWith { FeatureFlagSetting(true) }
+        val result = featureFlagRepo.first().featuresMap
+        assertEquals(expected, result)
+    }
 }
--- usecases/src/test/java/com/omar/retromp3recorder/bl/waveform/WavetableSummerTest.kt
+++ usecases/src/test/java/com/omar/retromp3recorder/bl/waveform/WavetableSummerTest.kt
@@ -1,11 +1,9 @@
 package com.omar.retromp3recorder.bl.waveform
 
 import org.junit.Assert.fail
-import org.junit.Ignore
 import org.junit.Test
 
 class WavetableSummerTest {
-    @Ignore("added to todo")
     @Test
     fun stub() = fail()
 }
