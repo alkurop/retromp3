09eb94582433f7f3ad20fd7c1bbfef91d0ea1efc
Alex Kuropiatnyk <alkurop@gmail.com>
2023-04-01 00:28:37 +0200

    Remove unused var

--- utils/data/src/main/java/com/omar/retromp3recorder/utils/domain/Constants.kt
+++ utils/data/src/main/java/com/omar/retromp3recorder/utils/domain/Constants.kt
@@ -2,6 +2,4 @@ package com.omar.retromp3recorder.utils.domain
 
 object Constants {
     const val MAIN_THREAD = "MAIN_THREAD"
-    const val PRECISION_PLAYER_TO_RECORDER_CONVERSION_MILLIS: Int = 10
-
 }
