6e44e29a74480b02e3460817ee643fda70c761f2
Alex Kuropiatnyk <alkurop@gmail.com>
2023-04-14 16:51:21 +0200

    Update crop menu requirements

--- app/src/main/java/com/omar/retromp3recorder/app/screens/home/components/menu/visibility_logic/file/CropFileMenuStateMapperFlow.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/screens/home/components/menu/visibility_logic/file/CropFileMenuStateMapperFlow.kt
@@ -2,24 +2,30 @@ package com.omar.retromp3recorder.app.screens.home.components.menu.visibility_lo
 
 import com.omar.retromp3recorder.app.screens.home.components.menu.MenuContract
 import com.omar.retromp3recorder.app.screens.home.components.menu.visibility_logic.MenuVisibilityMapperFlow
+import com.omar.retromp3recorder.bl.audio.progress.AudioState
+import com.omar.retromp3recorder.bl.audio.progress.AudioStateMapper
 import com.omar.retromp3recorder.domain.MenuPopup
 import com.omar.retromp3recorder.domain.PlayerRange
 import com.omar.retromp3recorder.storage.repo.local.PlayerProgressRepo
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import javax.inject.Inject
 
 class CropFileMenuStateMapperFlow @Inject constructor(
     private val playerProgressRepo: PlayerProgressRepo,
+    private val audioStateMapper: AudioStateMapper,
 ) : MenuVisibilityMapperFlow {
     override fun flow(): Flow<List<MenuContract.Item>> {
-        return playerProgressRepo.flow().map {
-            val range = it.value?.range?.notEmpty() ?: false
-
+        return combine(
+            playerProgressRepo.flow(),
+            audioStateMapper.flow()
+        ) { playerProgress, audioState ->
+            val hasRange = playerProgress.value?.range?.notEmpty() ?: false
+            val audioStateAllows = audioState is AudioState.Idle
             listOf(
                 MenuContract.Item.Popup(
                     MenuPopup.Crop,
-                    isEnabled = range
+                    isEnabled = hasRange && audioStateAllows
                 ),
             )
         }
