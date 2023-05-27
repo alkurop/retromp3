44af8a16f4f284962fd27a60ac0fe5a067abe336
Alex Kuropiatnyk <alkurop@gmail.com>
2023-05-27 21:17:17 +0200

    Update utils

--- app/src/main/java/com/omar/retromp3recorder/app/utils/UiUtils.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/utils/UiUtils.kt
@@ -3,9 +3,11 @@ package com.omar.retromp3recorder.app.utils
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.SharingStarted.Companion.Lazily
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 
-fun <T> Flow<T>.stateInViewModel(viewModel:ViewModel, default:T):StateFlow<T> =
-    this.stateIn(viewModel.viewModelScope, WhileSubscribed(), default)
+fun <T> Flow<T>.stateInViewModel(viewModel: ViewModel, default: T): StateFlow<T> =
+    this.stateIn(viewModel.viewModelScope, SharingStarted.Companion.Eagerly, default)
