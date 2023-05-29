f05c87fe78e9c8b6e4599de761822c4944ff840f
Alex Kuropiatnyk <alkurop@gmail.com>
2023-05-29 14:47:17 +0200

    Update notification text

--- app/src/main/java/com/omar/retromp3recorder/app/notification/AndroidLanguageDownloadNotificationSender.kt
+++ app/src/main/java/com/omar/retromp3recorder/app/notification/AndroidLanguageDownloadNotificationSender.kt
@@ -1,6 +1,7 @@
 package com.omar.retromp3recorder.app.notification
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
@@ -9,6 +10,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.omar.retromp3recorder.app.R
+import com.omar.retromp3recorder.app.screens.home.components.language.getDisplayNameRes
 import com.omar.retromp3recorder.domain.LanguageAvailability
 import com.omar.retromp3recorder.domain.LanguageState
 import com.omar.retromp3recorder.io.language.getFilename
@@ -24,50 +26,54 @@ class AndroidLanguageDownloadNotificationSender @Inject constructor(
 ) : LanguageDownloadNotificationSender {
 
     override suspend fun sendNotification(status: LanguageDownloadStatus) {
-
         languageAvailabilityRepo.updateItem(status.toAvailabilityStatus())
 
         if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.POST_NOTIFICATIONS
+                context, Manifest.permission.POST_NOTIFICATIONS
             ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
+        ) return
+
         val notificationId = status.language.getFilename().hashCode()
         when (status) {
             is LanguageDownloadStatus.FinishedSuccess -> {
                 NotificationManagerCompat.from(context).cancel(notificationId)
             }
+
             is LanguageDownloadStatus.FinishedWithError -> {
                 NotificationManagerCompat.from(context).cancel(notificationId)
             }
+
             is LanguageDownloadStatus.Progress -> {
-                val name = "VERBOSE_NOTIFICATION_CHANNEL_NAME"
-                val description = "VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION"
-                val importance = NotificationManager.IMPORTANCE_HIGH
-
-                val channel = NotificationChannel(FILE_DOWNLOAD_CHANNEL_ID, name, importance)
-                channel.description = description
-
-                // Add the channel
-                val notificationManager =
-                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
-
-                notificationManager?.createNotificationChannel(channel)
-
-                // Create the notification
-                val builder = NotificationCompat.Builder(context, FILE_DOWNLOAD_CHANNEL_ID)
-                    .setSmallIcon(R.mipmap.ic_launcher_foreground)
-                    .setContentTitle("NOTIFICATION_TITLE")
-                    .setContentText("${status.percent} %")
-                    .setSilent(true)
-                    .setOngoing(true)
-                    .setOnlyAlertOnce(true)
-
-                NotificationManagerCompat.from(context).notify(notificationId, builder.build())
+                val title = context.getString(R.string.language_download_notification_loading_title)
+                val text = context.getString(
+                    R.string.language_download_notification_loading_text,
+                    context.getString(status.language.getDisplayNameRes()),
+                    status.percent
+                )
+                showNotification(title, text, notificationId)
             }
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun showNotification(title: String, text: String, notificationId: Int) {
+        val channelName = context.getString(R.string.language_download_notification_title)
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(FILE_DOWNLOAD_CHANNEL_ID, channelName, importance)
+
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+        notificationManager?.createNotificationChannel(channel)
+
+        val builder = NotificationCompat.Builder(context, FILE_DOWNLOAD_CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_foreground).setContentTitle(title)
+            .setContentText(text)
+            .setSilent(true)
+            .setOngoing(true)
+            .setOnlyAlertOnce(true)
+
+        notificationManager?.notify(notificationId, builder.build())
     }
 }
 
